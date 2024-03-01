9bdc390dd8ec0e83490576b792b6f29e5f68e1b3
MohamedTlili <Mohamedtlili1995@hotmail.com>
2024-03-01 21:33:27 +0100

    experience-models

--- models/Experience.js
+++ models/Experience.js
@@ -2,22 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const experienceSchema = new Schema({
-    title: {
-      type: String,
-      require: true,
-    },
-    period: {
-      type: String,
-      require: true,
-    },
-    company: {
-      type: String,
-      require: true,
-    },
-    description: {
-      type: String,
-      require: true,
-    },
-  });
-  
-  module.exports = Experience = mongoose.model("experience", experienceSchema);
+  title: {
+    type: String,
+    require: true,
+  },
+  period: {
+    type: String,
+    require: true,
+  },
+  company: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = Experience = mongoose.model("experience", experienceSchema);
